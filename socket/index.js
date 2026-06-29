76b4f0d1ebdd46cf42e40e89479b52dee4bdc88b
Mohammad-Ali-Soomro <u2023326@giki.edu.pk>
2026-06-29 17:49:03 +0500

    Implement socket.io server at socket/index.js

---
+++ socket/index.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const socketIO = require("socket.io");
+const http = require("http");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello world from socket server!");
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  if (!users.some((user) => user.userId === userId)) {
+    users.push({ userId, socketId });
+  }
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (receiverId) => {
+  return users.find((user) => user.userId === receiverId);
+};
+
+const createMessage = ({ senderId, receiverId, text, images }) => {
+  return {
+    id: `${senderId}-${receiverId}-${Date.now()}`,
+    senderId,
+    receiverId,
+    text,
+    images,
+    seen: false,
+  };
+};
+
+const messages = {};
+
+io.on("connection", (socket) => {
+  console.log("a user is connected");
+
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  socket.on("sendMessage", ({ senderId, receiverId, text, images }) => {
+    const message = createMessage({ senderId, receiverId, text, images });
+    const user = getUser(receiverId);
+
+    if (!messages[receiverId]) {
+      messages[receiverId] = [message];
+    } else {
+      messages[receiverId].push(message);
+    }
+
+    io.to(user?.socketId).emit("getMessage", message);
+  });
+
+  socket.on("messageSeen", ({ senderId, receiverId, messageId }) => {
+    const user = getUser(senderId);
+
+    if (messages[senderId]) {
+      const message = messages[senderId].find(
+        (m) => m.receiverId === receiverId && m.id === messageId
+      );
+      if (message) {
+        message.seen = true;
+        io.to(user?.socketId).emit("messageSeen", {
+          senderId,
+          receiverId,
+          messageId,
+        });
+      }
+    }
+  });
+
+  socket.on("updateLastMessage", ({ lastMessage, lastMessagesId }) => {
+    io.emit("getLastMessage", {
+      lastMessage,
+      lastMessagesId,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
